37d6456fe3e503a041b335bbee6a82040bc86cdd
Erik Peteri <peteri.erik@gmail.com>
2024-04-20 16:51:45 +0300

    debu

--- cypress/e2e/anecdotes.cy.js
+++ cypress/e2e/anecdotes.cy.js
@@ -1,7 +1,6 @@
 describe('Anecdotes', function() {
   it('list is shown', function() {
     cy.visit('http://localhost:3000')
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.contains('Premature optimization is the root of all evil.')
   })
 })
