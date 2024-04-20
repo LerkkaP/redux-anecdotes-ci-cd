47ee45963043576c32a56777dc390357e232c332
Erik Peteri <peteri.erik@gmail.com>
2024-04-20 17:44:38 +0300

    continue debugging

--- cypress/e2e/anecdotes.cy.js
+++ cypress/e2e/anecdotes.cy.js
@@ -1,6 +1,7 @@
 describe('Anecdotes', function() {
   it('list is shown', function() {
     cy.visit('http://localhost:5000')
+    cy.get('body').should('contain', 'Premature optimization is the root of all evil.')
     //cy.contains('Premature optimization is the root of all evil.')
   })
 })
--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -3,8 +3,16 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/anecdotes'
 
 const getAll = async () => {
-  const response = await axios.get(baseUrl)
-  return response.data
+  try {
+    const response = await axios.get(baseUrl)
+    // eslint-disable-next-line no-console
+    console.log('getAll response:', response.data)
+    return response.data
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error fetching anecdotes:', error)
+    throw error
+  }
 }
 
 const createNew = async (content) => {
