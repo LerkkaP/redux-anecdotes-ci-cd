5000ff6f7cc55c8f7e0690777af55e7691e85c6f
Erik Peteri <peteri.erik@gmail.com>
2024-04-20 16:25:58 +0300

    update cypress github action and add timeout

--- cypress/e2e/anecdotes.cy.js
+++ cypress/e2e/anecdotes.cy.js
@@ -2,7 +2,6 @@ describe('Anecdotes', function() {
   it('list is shown', function() {
     cy.visit('http://localhost:3000')
     // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(1000)
     cy.contains('Premature optimization is the root of all evil.')
   })
 })
