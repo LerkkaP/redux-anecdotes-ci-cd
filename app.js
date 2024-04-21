091a9ce347effbfa1e3ada466a2e21ad96cd9a50
Erik Peteri <peteri.erik@gmail.com>
2024-04-21 16:46:28 +0300

    deploy app to fly manually

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 5000
 
-// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'dist')))
 
-// Handle API routes with json-server
 const jsonServer = require('json-server')
 app.use('/api', jsonServer.router('db.json'))
 
-// Serve the React app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
